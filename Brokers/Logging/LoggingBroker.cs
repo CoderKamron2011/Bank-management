adbe7d3edca26cb52a330bba31aba2c313ace971
CoderKamron2011 <hasanovaruxshona774@gmail.com>
2024-06-05 10:45:34 +0500

    Logging impliment

---
+++ Brokers/Logging/LoggingBroker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_management.Brokers.Logging
+{
+    internal class LoggingBroker: ILoggingBroker
+    {
+        public void LogError(string userMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(userMessage);
+            Console.ResetColor();
+        }
+        public void LogInformation(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
