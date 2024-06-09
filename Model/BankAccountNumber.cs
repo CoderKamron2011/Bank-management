e8fc7c63d5da50f5a998ff297c3c36c86bde9250
CoderKamron2011 <hasanovaruxshona774@gmail.com>
2024-06-09 13:32:14 +0500

    DATA: Initialize Customer and BankAcooountNumber

---
+++ Model/BankAccountNumber.cs
@@ -0,0 +1,12 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+//----------------------------------------
+
+namespace Bank_management.Model
+{
+    internal class BankAccountNumber
+    {
+        public decimal AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
