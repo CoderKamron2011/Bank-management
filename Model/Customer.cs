19153abd53df7f0064f7f430ea64953998555f9f
CoderKamron2011 <hasanovaruxshona774@gmail.com>
2024-06-17 13:41:05 +0500

    FOUNDATION: Impliment Bank Service and Validation all methods

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -7,7 +7,7 @@ namespace Bank_management.Model
     internal class Customer
     {
         public string Name { get; set; }
-        public decimal AccountNumber { get; set; }
+        public decimal AccountNumber { get; set; }  
         public decimal Balance { get; set; }
     }
 }
